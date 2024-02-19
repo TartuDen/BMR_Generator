d9f1b101058bb1bf982598f5443147e8fd2223d1
Pomog <thoryur@gmail.com>
2024-02-19 21:33:58 +0200

    replaceTextWithSelect

--- public/callFunc.js
+++ public/callFunc.js
@@ -10,12 +10,13 @@ function GetActivitiesList(equipmentType, callback) {
       {
         OperationType: "loading_of_solid",
         Content:
-          "Required amount of {material} is weighed on the balances {balances} using jug {jug}. Material is loaded into reactor {equipmentCode} via 60 mm flange port using funnel {funnel}. The 60 mm flange port is closed.",
+          "Required amount of {material} is weighed on the balances {balances} using jug {jug}. Material is loaded into reactor {reactor} via 60 mm flange port using funnel {funnel}. The 60 mm flange port is closed.",
         DurationMIN: "",
         DurationMAX: "",
         TemperatureMIN: "",
         TemperatureMAX: "",
-      },{
+      },
+      {
         OperationType: "loading_of_liquid",
         Content:
           "Required amount of {material} is weighed on the balances {balances} using jug {jug}. Using peristaltic pump  {peristaltic pump} and norprene hose {norprene hose}, {material} is pumped into reactor via liquid loading valve. Peristaltic pump is set to {setting}%. After loading is done, pump is stopped, hose is removed. The 60 mm flange port is closed. Hose is cleaned.",
@@ -23,7 +24,8 @@ function GetActivitiesList(equipmentType, callback) {
         DurationMAX: "",
         TemperatureMIN: "",
         TemperatureMAX: "",
-      },{
+      },
+      {
         OperationType: "dosing_of_liquid",
         Content:
           "Required amount of {material} is weighed on the balances {balances} using jug {jug}. Using peristaltic pump  {peristaltic pump} and norprene hose {norprene hose}, {material} is pumped into dosing system. Peristaltic pump is set to {setting}%. After loading is done, pump is stopped, hose is removed. Dosing system is closed. Hose is cleaned.",
@@ -31,7 +33,8 @@ function GetActivitiesList(equipmentType, callback) {
         DurationMAX: "",
         TemperatureMIN: "",
         TemperatureMAX: "",
-      },{
+      },
+      {
         OperationType: "creating_argon_flow",
         Content:
           "Argon line is connected to the argon port of reactor. The flow is set to {setting}l/min. The valve is opened. After required time is passed, the argon flow is closed.",
@@ -39,26 +42,26 @@ function GetActivitiesList(equipmentType, callback) {
         DurationMAX: "",
         TemperatureMIN: "",
         TemperatureMAX: "",
-      },{
+      },
+      {
         OperationType: "stirring_on",
-        Content:
-        "Stirring in reactor is turned ON. Set to {setting}rpm.",
+        Content: "Stirring in reactor is turned ON. Set to {setting}rpm.",
         DurationMIN: "",
         DurationMAX: "",
         TemperatureMIN: "",
         TemperatureMAX: "",
-      }, {
+      },
+      {
         OperationType: "stirring_off",
-        Content:
-        "Stirring in reactor is turned OFF.",
+        Content: "Stirring in reactor is turned OFF.",
         DurationMIN: "",
         DurationMAX: "",
         TemperatureMIN: "",
         TemperatureMAX: "",
-      }, {
+      },
+      {
         OperationType: "heating_on",
-        Content:
-        "Heating is turned ON. Set to {setting}°C.",
+        Content: "Heating is turned ON. Set to {setting}°C.",
         DurationMIN: "",
         DurationMAX: "",
         TemperatureMIN: "",
@@ -70,30 +73,60 @@ function GetActivitiesList(equipmentType, callback) {
   }, 200);
 }
 
-
-
-
 /**
- * Simulates the retrieval of a list of equipment for spicial equipmentType for testing purposes.
+ * Simulates the retrieval of a list of equipment for special equipmentType for testing purposes.
  *
- * @param {string} equipmentType - The type of equipment to retrieve form the DataBase
+ * @param {string} equipmentType - The type of equipment to retrieve from the DataBase
  * @param {function} callback - The callback function to execute after retrieving the equipment list.
  */
-function GetEquipmentList(equipmentType, callback) {
+function GetEquipmentListByType(equipmentType, callback) {
   setTimeout(function () {
-    var reactorList = [
-      { name: "reactor", code: "002-10" },
-      { name: "reactor", code: "002-11" },
-      { name: "reactor", code: "002-12" },
-      { name: "reactor", code: "002-13" },
-      { name: "reactor", code: "002-14" },
-      { name: "reactor", code: "002-15" },
-      { name: "reactor", code: "002-16" },
-    ];
+    var equipmentList;
+    switch (equipmentType) {
+      case "balances":
+        equipmentList = [
+          { name: "balances", code: "007-10" },
+          { name: "balances", code: "007-11" },
+          { name: "balances", code: "007-12" },
+        ];
+        break;
+      case "pump":
+        equipmentList = [
+          { name: "pump", code: "001-10" },
+          { name: "pump", code: "001-11" },
+          { name: "pump", code: "001-12" },
+        ];
+        break;
+      case "reactor":
+        equipmentList = [
+          { name: "reactor", code: "002-10" },
+          { name: "reactor", code: "002-11" },
+          { name: "reactor", code: "002-12" },
+          { name: "reactor", code: "002-13" },
+        ];
+        break;
+      case "jug":
+        equipmentList = [
+          { name: "jug", code: "tile" },
+          { name: "jug", code: "waste" },
+        ];
+        break;
+      case "funnel": // Adding support for "funnel"
+        equipmentList = [
+          { name: "funnel", code: "funnel-1" },
+          { name: "funnel", code: "funnel-2" },
+          { name: "funnel", code: "funnel-3" },
+        ];
+        break;
+      default:
+        // Handle unknown equipmentType or other cases
+        equipmentList = [];
+    }
     // Call the callback function with the retrieved data
-    callback(reactorList);
+    callback(equipmentList);
   }, 200);
 }
+
 
 /**
  * Simulates the retrieval of a list of equipment types for testing purposes.
@@ -119,15 +152,48 @@ function GetEquipmentType(callback) {
 function boldTextInBraces(text) {
   return text.replace(/\{([^}]+)\}/g, '<strong>{$1}</strong>');
 }
-function replaceTextWithSelect(text, materials) {
-  return text.replace(/\{([^}]+)\}/g, function(match, capturedText) {
-      var selectOptions = materials.map(function(material) {
-          return '<option value="' + material.name + '">' + material.name + '</option>';
-      }).join('');
-      return '<select class="form-control" onchange="saveSelectedItem(this); populateDescription()"><option value="">--select--</option>' + selectOptions + '</select>';
+
+/**
+ * Replace {placeHolders} in description text to option select list.
+ *
+ * @param {string} text - The description for operation.
+ */
+function replaceTextWithSelect(text) {
+  const regex = /\{([^}]+)\}/g;
+  let equipmentTypes = [];
+  let output = text;
+
+  let promises = [];
+
+  let match;
+  while ((match = regex.exec(text)) !== null) {
+    equipmentTypes.push(match[1]);
+  }
+
+  equipmentTypes.forEach((equipmentType) => {
+    let promise = new Promise((resolve) => {
+      GetEquipmentListByType(equipmentType, function (equipmentList) {
+        // Add default "--select--" option
+        const defaultOption = '<option value="">--select--</option>';
+        const selectOptions = equipmentList
+          .map(
+            (equipment) =>
+              `<option value="${equipment.code}">${equipment.code}</option>`
+          )
+          .join("");
+        const selectList = `<select name="${equipmentType}">${defaultOption}${selectOptions}</select>`;
+        output = output.replace(`{${equipmentType}}`, selectList);
+        resolve();
+      });
+    });
+
+    promises.push(promise);
+  });
+
+  return Promise.all(promises).then(() => {
+    return output;
   });
 }
-
 
 // Function to fetch data for the selected item
 function fetchSelectedItemData(selectElement) {
