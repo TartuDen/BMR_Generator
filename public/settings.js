ccb834923d0d91a065ad14df6bf1d794f64a775d
Yurii Panasuik <110051191+Pomog@users.noreply.github.com>
2024-02-21 17:37:56 +0200

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -1,6 +1,7 @@
 module.exports = {
     databaseServerUrl: 'http://database-server',
     equipmentApiEndpoint: '/api/equipment',
+    equipmentTypesApiEndpoint: '/api/equipment/types',
 };
 
 module.exports = settings;
