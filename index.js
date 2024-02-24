05198363123351c642c4c188ade097b514760272
dverves <denver1033@gmail.com>
2024-02-24 20:27:22 +0200

    create mock authorization, token creation and send it to the client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 // import morgan from "morgan";
 // import MyFunctions from "./funcs.js"
 // import { Equipment, Description, TypicalActivity, Material, Waste, Operation } from "./funcs.js";
@@ -7,19 +9,34 @@ import bodyParser from "body-parser";
 
 const port = 8080;
 const app = express();
-
+const mockUser = {// Mock user details
+    id: 1,
+    username: 'pomog',
+    email: 'pomog@dryg.com'
+}
+const secretKey = crypto.randomBytes(32).toString('hex');
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to generate JWT token
+function mockAuthentication(req, res, next) {
+    //Generate JWT token for the mock user
+    const token = jwt.sign({ user: mockUser }, secretKey, { expiresIn: '1H' });
+    // Attach the token to the request object
+    req.token = token;
+    // Call the next middleware
+    next();
+}
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 
-    res.status(200).render("index.ejs",{});
+    res.status(200).render("index.ejs", {token: req.token});
 });
 
-app.listen(port,(err)=>{
+app.listen(port, (err) => {
     if (err) {
         console.log(err);
         throw err;
