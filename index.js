205093431b1b6cfa2ba426f81035a4d3f0b58485
dverves <denver1033@gmail.com>
2024-03-09 00:21:16 +0200

    work on getting data from server and using for op table

--- index.js
+++ index.js
@@ -16,12 +16,37 @@ app.get("/",(req,res)=>{
     res.status(200).render("main_table.ejs",{});
 });
 
-app.post("/",(req,res)=>{
-    res.status(200).render("main_table.ejs",{});
-});
+app.post("/operation_table",(req,res)=>{
+    const {reactor1, reactor2, oven, m_pump1, 
+        m_pump2, p_pump1, p_pump2, o_pump, nutsche_filter, 
+        druck_filter, balances1, balances2} = req.body
+        let equipmentTypes = [];
+    if(reactor1!==""|| reactor2!==""){
+        equipmentTypes.push({name: "reactor"});
+    }
+    if (oven!==""){
+        equipmentTypes.push({name:"oven"});
+    }
+    if (m_pump1!==""|| m_pump2!==""){
+        equipmentTypes.push({name:"membrane_pump"});
+    }
+    if (p_pump1!==""|| p_pump2!==""){
+        equipmentTypes.push({name:"peristaltic_pump"});
+    }
+    if (o_pump!==""){
+        equipmentTypes.push({name:"oil_pump"});
+    }
+    if (nutsche_filter!==""){
+        equipmentTypes.push({name:"nutsche_filter"});
+    }
+    if (druck_filter!==""){
+        equipmentTypes.push({name:"druck_filter"});
+    }
+    if (balances1!==""||balances2!==""){
+        equipmentTypes.push({name:"balances"});
+    }
 
-app.get("/operation_table",(req,res)=>{
-    res.status(200).render("index.ejs",{});
+    res.status(200).render("index.ejs",{equipmentTypes});
 });
 
 app.listen(port,(err)=>{
