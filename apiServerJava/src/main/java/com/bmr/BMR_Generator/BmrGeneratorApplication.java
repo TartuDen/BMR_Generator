180da9665ddea08b83098ade263f3701d562d8e4
Yurii Panasuik <thoryur@gmail.com>
2024-04-19 22:08:51 +0300

    initial

---
+++ apiServerJava/src/main/java/com/bmr/BMR_Generator/BmrGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.bmr.BMR_Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmrGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmrGeneratorApplication.class, args);
+	}
+
+}
---
+++ apiServerJava/src/test/java/com/bmr/BMR_Generator/BmrGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmr.BMR_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmrGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
