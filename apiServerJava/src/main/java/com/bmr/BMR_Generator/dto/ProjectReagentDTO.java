efcaf3f0977466d5e4c0c425ba32b22069c9cded
Yurii Panasuik <thoryur@gmail.com>
2024-05-11 17:02:33 +0300

    ProjectReagent corrected

--- apiServerJava/src/main/java/com/bmr/BMR_Generator/dto/ProjectReagentDTO.java
+++ apiServerJava/src/main/java/com/bmr/BMR_Generator/dto/ProjectReagentDTO.java
@@ -1,6 +1,7 @@
 package com.bmr.BMR_Generator.dto;
 
 import com.bmr.BMR_Generator.entity.ProjectReagent;
+import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -16,8 +17,12 @@ public class ProjectReagentDTO {
     @NonNull
     private String name;
     
+    @NonNull
+    private Double mass;
+    
     public ProjectReagentDTO(ProjectReagent projectReagent) {
         this.tableID = projectReagent.getTableID();
         this.name = projectReagent.getName();
+        this.mass = projectReagent.getMass();
     }
 }
